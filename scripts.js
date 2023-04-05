c72527ef4413a2cae3078a5f178fbd3d81978a2c
nihil0r <brice.grivet.1995@gmail.com>
2023-04-05 18:37:19 +0200

    seconde page

--- scripts.js
+++ scripts.js
@@ -85,3 +85,67 @@ applyFiltersButton.addEventListener('click', () => {
     $('#filterModal').modal('hide');
 });
 
+// page 2
+
+// Fonction pour récupérer les paramètres de l'URL
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    const results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
+  
+  // Récupérez le paramètre 'categorie' de l'URL
+  const categorie = getUrlParameter('categorie');
+  
+  // Chargez les données pour la catégorie sélectionnée
+  chargerDonnees(categorie);
+  
+  function chargerDonnees(categorie) {
+    // Chargez les données en fonction de la catégorie
+    // (vous pouvez les charger depuis un fichier JSON ou via une API)
+  
+    // Exemple de données
+    const donnees = [
+      {
+        nom: "Variété 1",
+        saisonnalite: "Janvier - Mars",
+        localite: "France",
+        prix: "2,99 €/kg",
+        typeAgriculture: "Biologique"
+      },
+      // Ajoutez d'autres variétés ici
+    ];
+  
+    // Sélectionnez l'élément HTML où vous voulez afficher les données
+    const conteneur = document.getElementById('conteneur');
+  
+    // Parcourez les données et créez les éléments HTML
+    for (const produit of donnees) {
+      const divProduit = document.createElement('div');
+      const img = document.createElement('img');
+      const divDetails = document.createElement('div');
+      const divGraphique = document.createElement('div');
+  
+    // Ajoutez les attributs et le contenu aux éléments créés
+    img.src = `images/${categorie}/${produit.nom}.jpg`; // Assurez-vous d'avoir les images correspondantes dans votre dossier 'images'
+    img.alt = produit.nom;
+
+    divDetails.innerHTML = `
+      <h3>${produit.nom}</h3>
+      <p>Saisonnalité: ${produit.saisonnalite}</p>
+      <p>Localité: ${produit.localite}</p>
+      <p>Prix: ${produit.prix}</p>
+      <p>Type d'agriculture: ${produit.typeAgriculture}</p>
+    `;
+
+    // Chargez le graphique matplotlib ici
+    // Vous pouvez utiliser une bibliothèque comme mpld3 (https://mpld3.github.io/) pour afficher les graphiques Matplotlib dans une page web
+
+    // Ajoutez les éléments créés au conteneur
+    divProduit.appendChild(img);
+    divProduit.appendChild(divDetails);
+    divProduit.appendChild(divGraphique);
+    conteneur.appendChild(divProduit);
+  }
+}
